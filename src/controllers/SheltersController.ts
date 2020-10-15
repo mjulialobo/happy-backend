e37547844a0397b769d1452f57e761b5d7a72ad0
mjulialobo <mjulialobo23@gmail.com>
2020-10-15 00:37:40 -0300

    connection with front

--- src/controllers/SheltersController.ts
+++ src/controllers/SheltersController.ts
@@ -49,7 +49,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: open_on_weekends =='true',
             images
         }
 
@@ -67,6 +67,7 @@ export default {
 
 
         })
+
          
         await schema.validate(data, {
             abortEarly: false,
